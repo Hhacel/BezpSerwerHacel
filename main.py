e280b409e24a66f623302d7e99dab96a4296abc6
hhacel <hhacel@gmail.com>
2023-11-12 13:26:00 +0100

    RSA added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import base64
 import uuid
 from datetime import datetime
 
@@ -14,6 +17,7 @@ def register():
     login = data.get('username')
     password = data.get('password')
     username = data.get('username', login)
+    users_key = data.get('public_key')
 
     if not login or not password:
         return jsonify({'error': 'Missing login or password'}), 400
@@ -21,12 +25,19 @@ def register():
         return jsonify({'error': 'User already exists'}), 409
 
     token = str(uuid.uuid4())
+
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+
     users[login] = {
         'password': password,
         'username': username,
-        'token': token
+        'token': token,
+        'private_key': private_key,
+        'public_key': users_key
     }
-    return jsonify({'token': token}), 201
+    return jsonify({'token': token, 'public_key': public_key}), 201
 
 
 @app.route('/login', methods=['POST'])
@@ -57,10 +68,14 @@ def post_message():
     if not user_info:
         return jsonify({'error': 'Invalid token'}), 403
 
+    private_key = RSA.import_key(users[data['receiver']]['private_key'])
+    cipher = PKCS1_v1_5.new(private_key)
+    decrypted_message = cipher.decrypt(base64.b64decode(data['text']))
+
     messages.append({
         'sender': user_info['username'],
         'receiver': data['receiver'],
-        'text': data['text'],
+        'text': decrypted_message.decode('utf-8'),
         'datetime': datetime.now().isoformat()  # ISO 8601 format
     })
     return jsonify({'message': 'Message sent successfully'}), 201
@@ -77,10 +92,24 @@ def get_message():
     if not user_info:
         return jsonify({'error': 'Invalid token'}), 403
 
-    users_messages = [item for item in messages if
+    user_messages = [item for item in messages if
                       item['sender'] == user_info['username'] or item['receiver'] == user_info['login']]
 
-    return jsonify(users_messages)
+    encrypted_messages = []
+    public_key = RSA.import_key(user_info['public_key'])
+    cipher = PKCS1_v1_5.new(public_key)
+
+    for msg in user_messages:
+        encrypted_text = cipher.encrypt(msg['text'].encode('utf-8'))
+        encrypted_message = base64.b64encode(encrypted_text).decode('utf-8')
+        encrypted_messages.append({
+            'sender': msg['sender'],
+            'receiver': msg['receiver'],
+            'text': encrypted_message,
+            'datetime': msg['datetime']
+        })
+
+    return jsonify(encrypted_messages)
 
 
 if __name__ == '__main__':
